c614ff95864957117e18b932bcc981fe1e9b6cc7
JBRKR000 <kozimorjakub@gmail.com>
2025-01-07 19:57:17 +0100

    Modified the names of the admin and user panels. Added fields for email address and phone number in the accident report page.

--- src/app/components/UserContent.tsx
+++ src/app/components/UserContent.tsx
@@ -12,64 +12,62 @@ type Props = {};
 
 const UserContent = (props: Props) => {
   return (
-    <>
-      <UserNavbar />
-      <div id="main-container" className="flex flex-col bg-gray-100 dark:bg-neutral-800 min-h-screen">
-        <main className="flex-grow p-8 flex justify-center">
-          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 gap-y-2 mt-14">
-            {/* Kafelek 1 */}
-            <Link href="/payments" className="text-blue-600 font-bold dark:text-emerald-500 block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-                <MdPayments className='w-56 h-56 pt-5'/>
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white ">Zaległe płatności</h3>
-                
-              </div>
+      <>
+        <UserNavbar />
+        <div id="main-container" className="flex flex-col bg-gray-100 dark:bg-neutral-800 min-h-screen">
+          <main className="flex-grow p-8 flex justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 gap-y-2 mt-14">
+              {/* Kafelek 1 */}
+              <Link href="/payments" className="text-blue-600 font-bold dark:text-emerald-500 block">
+                <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <MdPayments className='w-56 h-56 pt-5'/>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Należności do Uregulowania</h3>
+                </div>
               </Link>
               {/* Kafelek 2 */}
               <Link href="/user_panel/contact" className="text-blue-600 font-bold dark:text-emerald-500  block">
-                <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-                <FaPhone className='w-52 h-52 pt-5'/>
-                  <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white">Kontakt</h3>
+                <div
+                    className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <FaPhone className='w-52 h-52 pt-5'/>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Dane Kontaktowe Administracji</h3>
                 </div>
               </Link>
               {/* Kafelek 3 */}
               <Link href="/user_panel/report" className="text-blue-600 font-bold dark:text-emerald-500 block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-              <MdError className='w-60 h-60  pt-5'/>
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white">Zgłoś problem</h3>
-                
-
-              </div>
+                <div
+                    className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <MdError className='w-60 h-60  pt-5'/>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Zgłoszenia Usterek i Problemów</h3>
+                </div>
               </Link>
               {/* Kafelek 4 */}
               <Link href="/user_panel/readings" className="text-blue-600 font-bold dark:text-emerald-500 block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-              <IoWater className='w-60 h-60'/>
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white">Odczyty liczników</h3>
-                
-              </div>
+                <div
+                    className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <IoWater className='w-60 h-60'/>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Stan Wodomierzy</h3>
+                </div>
               </Link>
               {/* Kafelek 5 */}
               <Link href="/user_panel/paid_invoices" className="text-blue-600 font-bold dark:text-emerald-500 block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-                <FaHistory className='w-56 h-56 pt-5'/>
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white">Historia transakcji</h3>
-                
-              </div>
+                <div
+                    className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <FaHistory className='w-56 h-56 pt-5'/>
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Historia Transakcji</h3>
+                </div>
               </Link>
               {/* Kafelek 6 */}
               <Link href="/user_panel/manage_account" className="text-blue-600 font-bold dark:text-emerald-500  block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-              <FaRegUser  className='w-60 h-60' />
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white">Zarządzaj kontem</h3>
-                 
-              </div>
+                <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
+                  <FaRegUser  className='w-60 h-60' />
+                  <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Ustawienia Konta Użytkownika</h3>
+                </div>
               </Link>
-          </div>
-        </main>
-      </div>
-      <UserFooter/>
-    </>
+            </div>
+          </main>
+        </div>
+        <UserFooter/>
+      </>
   );
 };
 
