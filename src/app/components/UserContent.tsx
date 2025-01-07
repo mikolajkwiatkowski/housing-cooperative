3ad30e5df4edc7f7932f5e8e9ab42ec5a6490b2c
Mikolaj Kwiatkowski <jestemczarny2137@gmail.com>
2025-01-07 20:07:56 +0100

    sperma

--- src/app/components/UserContent.tsx
+++ src/app/components/UserContent.tsx
@@ -12,20 +12,6 @@ type Props = {};
 
 const UserContent = (props: Props) => {
   return (
-<<<<<<< HEAD
-    <>
-      <UserNavbar />
-      <div id="main-container" className="flex flex-col bg-gray-100 dark:bg-neutral-800 min-h-screen">
-        <main className="flex-grow p-8 flex justify-center mt-5">
-          <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4 gap-y-2 mt-14">
-            {/* Kafelek 1 */}
-            <Link href="/payments" className="text-blue-600 font-bold dark:text-emerald-500 block">
-              <div className="bg-white hover:bg-stone-200 p-6 rounded-lg shadow-xl dark:bg-neutral-900 dark:hover:bg-neutral-700 flex flex-col items-center justify-between h-[24rem] w-[24rem]">
-                <MdPayments className='w-56 h-56 pt-5'/>
-                <h3 className="text-2xl font-semibold text-gray-900 mb-4 dark:text-white ">Zaległe płatności</h3>
-                
-              </div>
-=======
       <>
         <UserNavbar />
         <div id="main-container" className="flex flex-col bg-gray-100 dark:bg-neutral-800 min-h-screen">
@@ -37,7 +23,6 @@ const UserContent = (props: Props) => {
                   <MdPayments className='w-56 h-56 pt-5'/>
                   <h3 className="text-xl font-semibold text-gray-900 mb-4 dark:text-white">Należności do Uregulowania</h3>
                 </div>
->>>>>>> c614ff95864957117e18b932bcc981fe1e9b6cc7
               </Link>
               {/* Kafelek 2 */}
               <Link href="/user_panel/contact" className="text-blue-600 font-bold dark:text-emerald-500  block">
